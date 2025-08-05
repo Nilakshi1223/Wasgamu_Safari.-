c0debb4f0cfab986fc39702d746af9adfdb34d51
Nilakshi1223 <nmiyurangika1223@gmail.com>
2025-08-05 12:20:55 +0530

    Wasgamu Safari commit4

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/NavBar";
 import ScrollToTop  from "./components/ScrollToTop";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
